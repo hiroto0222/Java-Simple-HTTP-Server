1a8b781bde4ddc0504c3d0452bec46a2ef47acd4
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-06-04 16:17:04 +0900

    feat: create TCP socket on ServerListenerThread

--- src/main/java/com/hiroto0222/httpserver/HttpServer.java
+++ src/main/java/com/hiroto0222/httpserver/HttpServer.java
@@ -2,6 +2,7 @@ package com.hiroto0222.httpserver;
 
 import com.hiroto0222.httpserver.config.Configuration;
 import com.hiroto0222.httpserver.config.ConfigurationManager;
+import com.hiroto0222.httpserver.core.ServerListenerThread;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,7 +16,7 @@ import java.net.Socket;
  *  - unencrypted traffic on 80
  *  -> read configuration files
  * 2. make TCP socket connection on port with client
- *  - makes connection with 1 client only
+ *  - make multiple connections using threads
  * 3. read request messages following HTTP protocols
  * 4. open/read files from FS based on request
  * 5. write response message following HTTP protocols
@@ -32,32 +33,13 @@ public class HttpServer {
         System.out.println("Using Port: " + conf.getPort());
         System.out.println("Using WebRoot: " + conf.getWebroot());
 
-        // create TCP socket on port
-        try (ServerSocket serverSocket = new ServerSocket(conf.getPort())) {
-            Socket socket = serverSocket.accept();
-
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
-
-            // TODO: read request
-
-            // write response
-            String html = """
-            <html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using Simple Java HTTP Server</h1></body></html>
-            """;
-            final String CRLF = "\n\r"; // ascii 13, 10
-            String response = "HTTP/1.1 200 OK" + CRLF + // Status line -> HTTP_VERSION RESPONSE_CODE RESPONSE_MESSAGE
-                              "Content-Length: " + html.getBytes().length + CRLF + // Header
-                              CRLF +
-                              html + // Body
-                              CRLF + CRLF;
-            outputStream.write(response.getBytes());
-
-            inputStream.close();
-            outputStream.close();
-            socket.close();
+        // create TCP socket on thread
+        ServerListenerThread serverListenerThread;
+        try {
+            serverListenerThread = new ServerListenerThread(conf.getPort(), conf.getWebroot());
+            serverListenerThread.start();
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 }
---
+++ src/main/java/com/hiroto0222/httpserver/core/ServerListenerThread.java
@@ -0,0 +1,49 @@
+package com.hiroto0222.httpserver.core;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerListenerThread extends Thread{
+    private int port;
+    private String webRoot;
+    ServerSocket serverSocket;
+
+    public ServerListenerThread(int port, String webRoot) throws IOException {
+        this.port = port;
+        this.webRoot = webRoot;
+        this.serverSocket = new ServerSocket(this.port);
+    }
+
+    @Override
+    public void run() {
+        try {
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            // TODO: read request
+
+            // write response
+            String html = """
+            <html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using Simple Java HTTP Server</h1></body></html>
+            """;
+            final String CRLF = "\n\r"; // ascii 13, 10
+            String response = "HTTP/1.1 200 OK" + CRLF + // Status line -> HTTP_VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                    "Content-Length: " + html.getBytes().length + CRLF + // Header
+                    CRLF +
+                    html + // Body
+                    CRLF + CRLF;
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
