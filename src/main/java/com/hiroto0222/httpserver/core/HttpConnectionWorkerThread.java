24a3a7dd62a20ac0b6a78b79cdb14c92d5155678
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-06-04 16:48:19 +0900

    feat: add multi threads to accept concurrent connections

---
+++ src/main/java/com/hiroto0222/httpserver/core/HttpConnectionWorkerThread.java
@@ -0,0 +1,49 @@
+package com.hiroto0222.httpserver.core;
+
+import com.hiroto0222.httpserver.HttpServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class HttpConnectionWorkerThread extends Thread {
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpServer.class);
+
+    private final Socket socket;
+
+    public HttpConnectionWorkerThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (InputStream inputStream = socket.getInputStream(); OutputStream outputStream = socket.getOutputStream()) {
+            // TODO: read request
+
+            // write response
+            String html = """
+                    <html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using Simple Java HTTP Server</h1></body></html>
+                    """;
+            final String CRLF = "\n\r"; // ascii 13, 10
+            String response = "HTTP/1.1 200 OK" + CRLF + // Status line -> HTTP_VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                    "Content-Length: " + html.getBytes().length + CRLF + // Header
+                    CRLF +
+                    html + // Body
+                    CRLF + CRLF;
+            outputStream.write(response.getBytes());
+
+            LOGGER.info("Connection processing finished.");
+        } catch (IOException e) {
+            LOGGER.error("Problem with communication, ", e);
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException _) {}
+            }
+        }
+    }
+}
--- src/main/java/com/hiroto0222/httpserver/core/ServerListenerThread.java
+++ src/main/java/com/hiroto0222/httpserver/core/ServerListenerThread.java
@@ -5,8 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -26,32 +24,22 @@ public class ServerListenerThread extends Thread{
     @Override
     public void run() {
         try {
-            Socket socket = serverSocket.accept();
+            while (serverSocket.isBound() && !serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
 
-            LOGGER.info(" * Connection Accepted: {}", socket.getInetAddress());
+                LOGGER.info(" * Connection Accepted: {}", socket.getInetAddress());
 
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
-
-            // TODO: read request
-
-            // write response
-            String html = """
-            <html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using Simple Java HTTP Server</h1></body></html>
-            """;
-            final String CRLF = "\n\r"; // ascii 13, 10
-            String response = "HTTP/1.1 200 OK" + CRLF + // Status line -> HTTP_VERSION RESPONSE_CODE RESPONSE_MESSAGE
-                    "Content-Length: " + html.getBytes().length + CRLF + // Header
-                    CRLF +
-                    html + // Body
-                    CRLF + CRLF;
-            outputStream.write(response.getBytes());
-
-            inputStream.close();
-            outputStream.close();
-            socket.close();
+                HttpConnectionWorkerThread workerThread = new HttpConnectionWorkerThread(socket);
+                workerThread.start();
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.error("Problem with communication, ", e);
+        } finally {
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                } catch (IOException _) {}
+            }
         }
     }
 }
